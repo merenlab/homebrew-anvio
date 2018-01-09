a5cadf2ae038d925cef01572d672a55624ef10f1
Özcan Esen <ozcanesen@gmail.com>
2018-01-09 16:08:17 -0600

    mcl bottle high sierra

--- Formula/mcl.rb
+++ Formula/mcl.rb
@@ -4,6 +4,11 @@ class Mcl < Formula
   url "https://micans.org/mcl/src/mcl-14-137.tar.gz"
   sha256 "b5786897a8a8ca119eb355a5630806a4da72ea84243dba85b19a86f14757b497"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d371a662ab31801a57443d2ddf4f568f64920576d989465a60f9b6033817f7be" => :high_sierra
+  end
+
   def install
     bin.mkpath
     system "./configure",
